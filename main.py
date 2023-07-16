8102ddc12485bec3a3edbaa7bcbc9ef375df14eb
samjwu <sjwu@ualberta.ca>
2023-07-16 11:25:52 -0600

    add load weights method

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import neuralnetwork
 
 config_file_path = "config/yolo.cfg"
 image_file_path = "data/test.png"
+weights_file_path = "data/yolov3.weights"
 blocks = fileutil.read_configuration(config_file_path)
 network_info, module_list = fileutil.create_modules(blocks)
 
 model = neuralnetwork.NeuralNetwork(config_file_path)
+model.load_weights(weights_file_path)
 model_input = fileutil.process_input_image(image_file_path)
 if torch.cuda.is_available():
     prediction = model(model_input.cuda(), True)
